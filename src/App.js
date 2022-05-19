4f4a0f4bef0a510c8a4028db5ec918244671264e
UWEGHREPHA EJIRO THANKGOD <ejineroz@pop-os.localdomain>
2022-05-19 22:26:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+// import BasicForm from "./components/BasicForm";
+import SignUp from "./components/SignUp";
+// import AdvancedForm from "./components/AdvancedForm";
+import SignIn from "./components/SignIn";
+import "./App.css";
 
 function App() {
+  const [view, setView] = useState("basic");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <nav>
+        <h3
+          onClick={() => setView("basic")}
+          style={{ color: view === "basic" ? "#fff" : "" }}
         >
-          Learn React
-        </a>
-      </header>
+          Sign Up
+        </h3>
+        <h3
+          onClick={() => setView("advanced")}
+          style={{ color: view === "advanced" ? "#fff" : "" }}
+        >
+          Sign In
+        </h3>
+      </nav>
+      {view === "basic" ? <SignUp /> : <SignIn />}
     </div>
   );
 }
